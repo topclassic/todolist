d892c30e244756ad6396a9ace4cceaff7444c180
Chotipat Poowongthanarat <top_classic@hotmail.com>
2017-06-30 02:25:20 +0700

    Design Header

---
+++ src/app/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export class Header extends React.Component{
+
+    render(){
+        return(
+            <nav>
+                <ul>
+                    <li><a href="" ><span className="glyphicon glyphicon-plus"></span></a></li>
+
+                </ul>
+
+            </nav>
+        )
+    }
+}
--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,19 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { Header } from "./components/header"
 
 class App extends React.Component {
-
-
-
+    render() {
+        return (
+            <div>
+                <div>
+                    <br/>
+                    <Header/>
+                </div>
+                <div>
+                </div>
+            </div>
+        )
+    }
 }
 ReactDOM.render(<div> <App/> </div>,document.getElementById('todoList'))
